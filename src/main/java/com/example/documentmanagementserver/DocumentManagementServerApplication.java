5eb8e48f1b861491ea0b48e360758fbd6359cb0c
michu-dunat <kontaktmichaldunat@gmail.com>
2021-11-01 19:32:06 +0100

    add initial project

---
+++ src/main/java/com/example/documentmanagementserver/DocumentManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.documentmanagementserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagementServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentManagementServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/documentmanagementserver/DocumentManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.documentmanagementserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentManagementServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
