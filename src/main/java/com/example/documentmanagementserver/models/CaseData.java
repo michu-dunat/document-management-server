9390c6fa90426f8533051f751223f50207e83cf1
michu-dunat <kontaktmichaldunat@gmail.com>
2021-11-09 20:53:47 +0100

    update case model

--- src/main/java/com/example/documentmanagementserver/models/CaseData.java
+++ src/main/java/com/example/documentmanagementserver/models/CaseData.java
@@ -21,4 +21,54 @@ public class CaseData {
     @OneToOne
     private ProceedingsSubject proceedingsSubject;
 
+    public CaseData(int id, AdversePartyData adversePartyData, ClientData clientData, CourtData courtData, ProceedingsSubject proceedingsSubject) {
+        this.id = id;
+        this.adversePartyData = adversePartyData;
+        this.clientData = clientData;
+        this.courtData = courtData;
+        this.proceedingsSubject = proceedingsSubject;
+    }
+
+    public CaseData() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public AdversePartyData getAdversePartyData() {
+        return adversePartyData;
+    }
+
+    public void setAdversePartyData(AdversePartyData adversePartyData) {
+        this.adversePartyData = adversePartyData;
+    }
+
+    public ClientData getClientData() {
+        return clientData;
+    }
+
+    public void setClientData(ClientData clientData) {
+        this.clientData = clientData;
+    }
+
+    public CourtData getCourtData() {
+        return courtData;
+    }
+
+    public void setCourtData(CourtData courtData) {
+        this.courtData = courtData;
+    }
+
+    public ProceedingsSubject getProceedingsSubject() {
+        return proceedingsSubject;
+    }
+
+    public void setProceedingsSubject(ProceedingsSubject proceedingsSubject) {
+        this.proceedingsSubject = proceedingsSubject;
+    }
 }
